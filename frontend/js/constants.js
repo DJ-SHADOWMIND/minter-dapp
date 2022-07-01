eab5e41fc885f0058eb75e044c29ef0140dc0fe0
DJ SHADOWMIND <submissions@djshadowmind.com>
2022-07-01 15:15:17 -0700

    more

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,23 +3,23 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the 'Alien Grunts'' NFT Project!!";
 const welcome_h2 = "Connect to Coinbase or MetaMask to Get Started";
-const welcome_p = "The 'Aliens America' NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href='https://youtube.com/codestackr'  target='_blank' rel='noopener noreferrer'>codeSTACKr</a> YouTube channel.'";
-const h1_presale_coming_soon = "Alien America NFT Drop Coming Soon!!";
+const welcome_p = "The 'Alien Grunts' NFT Project is the second stop on DJ SHADOWMIND's NFT Roadmap.";
+const h1_presale_coming_soon = "Alien Grunts NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Mind Control That Alien Grunt Guardian!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const h1_public_mint = "Mind Control That Alien Grunt!";
+const h2_presale_coming_soon = "Pre-Sale Mint Countdown";
+const h2_presale_mint = "Public Mint Countdown";
+const p_presale_coming_soon = "Almost There. Visit DJShadowmind.Com for more Music and NFT Info";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale.";
+const p_presale_mint_already_minted = "You've already claimed your maximum mints. Thank you!";
 const p_public_mint = "Alien Grunts Public Mint Has Started";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const button_presale_coming_soon = "Visit Us For More Information on Twitter";
+const button_presale_mint_whitelisted = "Mint Your Alien Grunts NFT";
+const button_presale_mint_not_whitelisted = "Visit Us For More Information on Twitter";
+const button_presale_already_minted = "Visit Us For More Information on Twitter";
+const button_public_mint = "Mint Your Alien Grunt NFT";
+const mint_failed = "Minting failed. There was a rip in the time/space continuum. Please try again.";
 
 const abi = [
   {
